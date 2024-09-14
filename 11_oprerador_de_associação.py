49ef374d9d65e15285ae4081ee561834c0138702
ArisAioros <souaurelice@hotmail.com>
2024-09-14 16:24:51 -0300

    Continuando01 13/09

--- 11_oprerador_de_associação.py
+++
@@ -1,7 +0,0 @@
-# Existe diferença entre letras Maiuscula e letras Minuscula
-frutas = ["limão", "uva"]
-curso = ("curso de python")
-
-print("laranja" not in frutas)
-print("limão" in frutas)
-print("python" in curso)
