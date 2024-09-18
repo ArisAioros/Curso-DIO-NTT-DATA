eeeed074700641218d91340fb4a447783bcbf8cd
ArisAioros <souaurelice@hotmail.com>
2024-09-18 10:30:10 -0300

    Continuando 18/09

--- 04_Funçôes_Python/04_Args_Kwargs.py
+++ 04_Funçôes_Python/04_Args_Kwargs.py
@@ -1,3 +1,16 @@
+# Importante é se tem um asteristico ou dois asteristico de pois colocar qualque nome.
 def exibir_poema(data_extenso, *args, **kwargs):
     texto = "\n".join(args)
-    meta_dados = "\n".join([])
+    meta_dados = "\n".join([f"{chave.title()}: {valor}" for chave, valor in kwargs.items()])
+    mensagem = f"{data_extenso}\n\n{texto}\n\n{meta_dados}"
+    print(mensagem)
+
+
+exibir_poema(
+    "Sexta-feira 26 de agosto",
+    "Zen do Python",
+    "Bonito é melhor que feio.",
+    "Explicito é melhor que implícito.",
+    autor= "Tin Peters",
+    ano= 1999,
+)
